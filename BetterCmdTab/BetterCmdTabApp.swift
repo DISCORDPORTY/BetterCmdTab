918468c03735ef8d98bf286fe00fa81c1e54a837
rokartur <rokartur@icloud.com>
2026-05-22 13:04:55 +0200

    Initial Commit

---
+++ BetterCmdTab/BetterCmdTabApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterCmdTabApp.swift
+//  BetterCmdTab
+//
+//  Created by artur on 22/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct BetterCmdTabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BetterCmdTabTests/BetterCmdTabTests.swift
@@ -0,0 +1,18 @@
+//
+//  BetterCmdTabTests.swift
+//  BetterCmdTabTests
+//
+//  Created by artur on 22/05/2026.
+//
+
+import Testing
+
+struct BetterCmdTabTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
